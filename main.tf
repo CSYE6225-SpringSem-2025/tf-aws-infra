a7fd09a738d9e9d5e7e4ce95a391ed7cf41c8098
saurabhgangal <gangal.s@northeastern.edu>
2025-02-19 17:05:10 -0500

    test fail

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "aws_route_table" "private" {
 }
 
 # Route Table Associations
-resource "aws_route_table_association" "public" {
+resource "aws_route_tabe_association" "public" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
