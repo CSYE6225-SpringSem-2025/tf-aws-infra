54c655b059dc0297b3848dd94dd0d7d7dc7c6d70
saurabhgangal <gangal.s@northeastern.edu>
2025-02-25 16:48:44 -0500

    check failure

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ data "aws_availability_zones" "available" {
 }
 
 # VPC
-resource "aws_vpc" "main" {
+  resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
