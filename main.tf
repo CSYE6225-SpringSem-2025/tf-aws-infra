13d304792e80713715c29d5516ff8fab46512674
saurabhgangal <gangal.s@northeastern.edu>
2025-02-25 16:56:02 -0500

    check failure3

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_internet_gateway" "main" {
 
 # Public Subnets
 resource "aws_subnet" "public" {
-  count             = length(var.public_subnet_cidrss)
+  count             = length(var.public_subnet_cidrs)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[count.index]
   availability_zone = data.aws_availability_zones.available.names[count.index]
